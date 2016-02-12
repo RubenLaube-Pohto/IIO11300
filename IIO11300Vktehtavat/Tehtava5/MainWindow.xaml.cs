b3ed51432a2d8c971e2ac04d571e6b60f4ee9334
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-02-12 10:59:20 +0200

    Create main UI

--- IIO11300Vktehtavat/Tehtava5/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava5/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Tehtava5
         {
             InitializeComponent();
         }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
