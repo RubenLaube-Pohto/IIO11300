024e20f7ff59a8ab9c4f366da89c4823484ec687
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-02-25 17:06:07 +0200

    Finish exercise 7

--- IIO11300Vktehtavat/Tehtava7/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava7/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tehtava7
 {
@@ -27,36 +16,53 @@ namespace Tehtava7
 
         private void txtPassword_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (txtPassword.Text.Length <= 0)
+            string password = txtPassword.Text;
+            int total = password.Length;
+            int upperTotal = CountUpper(password);
+            int lowerTotal = CountLower(password);
+            int numberTotal = CountNumber(password);
+            int specialTotal = total - upperTotal - lowerTotal - numberTotal;
+
+            txtLength.Text = "Merkkejä: " + total;
+            txtUppercase.Text = "Isoja kirjaimia: " + upperTotal;
+            txtLowercase.Text = "Pieniä kirjaimia: " + lowerTotal;
+            txtNumbers.Text = "Numeroita: " + numberTotal;
+            txtSpecial.Text = "Erikoismerkkejä: " + specialTotal;
+
+            int types = 0;
+            if (upperTotal != 0) types++;
+            if (lowerTotal != 0) types++;
+            if (numberTotal != 0) types++;
+            if (specialTotal != 0) types++;
+
+            if (total == 0)
             {
-                txtLength.Text = ". . .";
-                txtUppercase.Text = ". . .";
-                txtLowercase.Text = ". . .";
-                txtNumbers.Text = ". . .";
-                txtSpecial.Text = ". . .";
+                txtPassword_Strength.Background = Brushes.LightGray;
                 txtPassword_Strength.Text = "Anna salasana";
             }
-            else
+            else if (total < 8 || types == 1)
             {
-                string password = txtPassword.Text;
-                int total = password.Length;
-                int upperTotal = CountUpper(password);
-                int lowerTotal = CountLower(password);
-                int numberTotal = CountNumber(password);
-                int specialTotal = total - upperTotal - lowerTotal - numberTotal;
-
-                txtLength.Text = "Merkkejä: " + total;
-                txtUppercase.Text = "Isoja kirjaimia: " + upperTotal;
-                txtLowercase.Text = "Pieniä kirjaimia: " + lowerTotal;
-                txtNumbers.Text = "Numeroita: " + numberTotal;
-                txtSpecial.Text = "Erikoismerkkejä: " + specialTotal;
-
-                /*
-                laske, kuinka monta 'ehtoa' täyttyy
-                switch/case
-                    maalaile tausta
-                    päivitä teksti
-                */
+                // BAD
+                txtPassword_Strength.Background = Brushes.Orange;
+                txtPassword_Strength.Text = "Bad";
+            }
+            else if (total < 12 && types >= 2)
+            {
+                // FAIR
+                txtPassword_Strength.Background = Brushes.Yellow;
+                txtPassword_Strength.Text = "Fair";
+            }
+            else if (total < 16 && types >= 3)
+            {
+                // MODERATE
+                txtPassword_Strength.Background = Brushes.LightGreen;
+                txtPassword_Strength.Text = "Moderate";
+            }
+            else if (total >= 16 && types == 4)
+            {
+                // GOOD
+                txtPassword_Strength.Background = Brushes.Green;
+                txtPassword_Strength.Text = "Good";
             }
         }
 
