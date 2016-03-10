5d9c10d82a4844dcf6e3e5571817c0dfc88583e3
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-03-10 09:23:09 +0200

    Continue H6

--- IIO11300Harjoitukset/H6DataBinding/Hockey.cs
+++ IIO11300Harjoitukset/H6DataBinding/Hockey.cs
@@ -1,10 +1,83 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace H6DataBinding
 {
-    public class HockeyPlayer
+    public static class TestHockeyBench
     {
+        public static List<HockeyPlayer> Get3Players()
+        {
+            List<HockeyPlayer> players = new List<HockeyPlayer>();
+            players.Add(new HockeyPlayer("Teemu Selänne", "8"));
+            players.Add(new HockeyPlayer("Jarkko Immonen", "26"));
+            players.Add(new HockeyPlayer("Ville Peltonen", "16"));
+            return players;
+        }
+    }
+    public class HockeyPlayer : INotifyPropertyChanged
+    {
+        #region VARIABLES
+        private string name;
+        private string number;
+        #endregion
+        #region PROPERTIES
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                Notify("Name");
+                Notify("NameAndNumber");
+            }
+        }
+        public string Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+                Notify("Number");
+                Notify("NameAndNumber");
+            }
+        }
+        public string NameAndNumber
+        {
+            get
+            {
+                return name + " #" + number;
+            }
+        }
+        #endregion
+        #region CONSTRUCTORS
+        public HockeyPlayer()
+        {
+            name = "";
+            number = "";
+        }
+        public HockeyPlayer(string name, string number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+        #endregion
+        #region INTERFACE
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        void Notify(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+        #endregion
     }
     public class HockeyTeam
     {
---
+++ IIO11300Harjoitukset/H6DataBinding/PlayerWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace H6DataBinding
+{
+    /// <summary>
+    /// Interaction logic for PlayerWindow.xaml
+    /// </summary>
+    public partial class PlayerWindow : Window
+    {
+        List<HockeyPlayer> players;
+        int index;
+
+        public PlayerWindow()
+        {
+            InitializeComponent();
+            InitMyStuff();
+        }
+
+        private void InitMyStuff()
+        {
+            players = TestHockeyBench.Get3Players();
+            dgPlayers.ItemsSource = players;
+            index = 0;
+            SetData();
+        }
+
+        private void SetData()
+        {
+            spLeft.DataContext = players[index];
+        }
+
+        private void dgPlayers_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if ((dgPlayers.SelectedIndex > -1) && (dgPlayers.SelectedIndex < players.Count)) 
+            {
+                index = dgPlayers.SelectedIndex;
+                SetData();
+            }
+        }
+    }
+}
