487681acb2b1ea2df9c76a9cd8f909f0c5d4e917
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-03-10 10:53:05 +0200

    Finish H7

--- IIO11300Harjoitukset/H7ADONETConsole/Program.cs
+++ IIO11300Harjoitukset/H7ADONETConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,50 @@ namespace H7ADONETConsole
     {
         static void Main(string[] args)
         {
-            // 1. Create connection
-            // 2. Create command and execute it
-            // 3. Loop through results
+            try
+            {
+                // 1. Create connection
+                string connStr = H7ADONETConsole.Properties.Settings.Default.Database;
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+
+                    // 2. Create command and execute it
+                    string sql = "SELECT asioid, lastname, firstname, date FROM presences WHERE asioid='H8871'";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    SqlDataReader sdr = cmd.ExecuteReader();
+
+                    // 3. Loop through results
+                    if (sdr.HasRows)
+                    {
+                        int count = 0;
+                        while (sdr.Read())
+                        {
+                            string id = sdr.GetString(0);
+                            string lastname = sdr.GetString(1);
+                            string firstname = sdr.GetString(2);
+                            string date = sdr.GetDateTime(3).ToShortDateString();
+                            Console.WriteLine("Läsnäolosi {0} {1} {2} {3}", id, firstname, lastname, date);
+                            count++;
+                        }
+                        Console.WriteLine("Tulostettu {0} läsnäoloa.", count);
+                    }
+
+                    // 4. Close the connection (With 'using' should close automatically.
+                    // Without 'using' would be better to close in 'finally'-block)
+                    sdr.Close();
+                    conn.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to close...");
+                Console.ReadKey();
+            }
         }
     }
 }
