8bc35cffca3b6f1813e975499df1fb35e019f4a9
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-03-10 10:07:30 +0200

    Begin H7

---
+++ IIO11300Harjoitukset/H7ADONETConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H7ADONETConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Create connection
+            // 2. Create command and execute it
+            // 3. Loop through results
+        }
+    }
+}
