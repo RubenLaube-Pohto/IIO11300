1592311d53397c2437053a5133953a9aa41325f3
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-03-10 11:09:47 +0200

    Finish H8

--- IIO11300Harjoitukset/H8ADONETWPF/MainWindow.xaml.cs
+++ IIO11300Harjoitukset/H8ADONETWPF/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +25,34 @@ namespace H8ADONETWPF
         public MainWindow()
         {
             InitializeComponent();
+            GetData();
+        }
+
+        private void GetData()
+        {
+            try
+            {
+                // Connect
+                using (SqlConnection conn = new SqlConnection(H8ADONETWPF.Properties.Settings.Default.Database))
+                {
+                    // Use data adapter to link datasource to element
+                    string sql = "SELECT asioid, lastname, firstname, date FROM presences WHERE asioid='H8871'";
+                    SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                    DataTable dt = new DataTable("Presences");
+                    da.Fill(dt);
+
+                    // Bind to UI control
+                    dgMyGrid.DataContext = dt;
+
+                    // Close (to be safe)
+                    conn.Close();
+                }
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
     }
 }
