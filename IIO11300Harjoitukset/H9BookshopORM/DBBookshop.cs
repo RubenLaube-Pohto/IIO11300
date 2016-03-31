095de3710f93b6550ff610b16e4cd2e9497a9feb
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-03-31 08:07:27 +0300

    Add exercise H9

---
+++ IIO11300Harjoitukset/H9BookshopORM/DBBookshop.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H9BookshopORM
+{
+  public class DBBookshop
+  {
+    public static DataTable GetTestData()
+    {
+      //luodaan testausta varten oikeanlainen DataTable
+      DataTable dt = new DataTable();
+      //sarakkeiden määrittely
+      dt.Columns.Add("id", typeof(int));
+      dt.Columns.Add("name", typeof(string));
+      dt.Columns.Add("author", typeof(string));
+      dt.Columns.Add("country", typeof(string));
+      dt.Columns.Add("year", typeof(int));
+      //rivit eli tietueet
+      dt.Rows.Add(11, "Pekka Lipposen seikkailut", "Outsider", "Suomi", 1950);
+      dt.Rows.Add(12, "Lucky Luke", "René Coscinny", "Belgia", 1946);
+      return dt;
+    }
+    public static DataTable GetBooks(string connStr)
+    {
+      try
+      {
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+          string sql = "SELECT id, name, author, country, year FROM books";
+          conn.Open();
+          SqlCommand cmd = new SqlCommand(sql, conn);
+          SqlDataAdapter da = new SqlDataAdapter(cmd);
+          DataTable dt = new DataTable("Books");
+          da.Fill(dt);
+          conn.Close();
+          return dt;
+        }
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+    public static int UpdateBook(string connStr, int id, string name, string author, string country, int year)
+    {
+      try
+      {
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+          conn.Open();
+          //HUOM: käytetään SQL-kyselyn parametrejä kahdesta syystä:
+          //1: heittomerkki (hipsa) engl. apostrophe esim nimessä O'Hara
+          //2: tietoturva: parametrisoidut kyselyt ovat tietoturvallisempia
+          string sql = string.Format(
+            "UPDATE books SET name=@Nimi, author=@Kirjailija, country='{3}', year={4} WHERE id={0}",
+            id, name, author, country, year);
+          SqlCommand cmd = new SqlCommand(sql, conn);
+          //lisätään komennolla parametrit
+          SqlParameter sp;
+          sp = new SqlParameter("Nimi", SqlDbType.NVarChar);
+          sp.Value = name;
+          cmd.Parameters.Add(sp);
+          sp = new SqlParameter("Kirjailija", SqlDbType.NVarChar);
+          sp.Value = author;
+          cmd.Parameters.Add(sp);
+          //suoritetaan kysely kantaan
+          int lkm = cmd.ExecuteNonQuery();
+          conn.Close();
+          return lkm;
+        }
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+    public static int InsertBook(string connStr, string name, string author, string country, int year)
+    {
+      try
+      {
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+          conn.Open();
+          //HUOM: käytetään SQL-kyselyn parametrejä kahdesta syystä:
+          //1: heittomerkki (hipsa) engl. apostrophe esim nimessä O'Hara
+          //2: tietoturva: parametrisoidut kyselyt ovat tietoturvallisempia
+          string sql = 
+            "INSERT INTO books (name, author, country, year) VALUES (@Nimi, @Kirjailija, @Maa, @Vuosi)";
+          SqlCommand cmd = new SqlCommand(sql, conn);
+          //lisätään komennolla parametrit
+          cmd.Parameters.AddWithValue("@Nimi", name);
+          cmd.Parameters.AddWithValue("@Kirjailija", author);
+          cmd.Parameters.AddWithValue("@Maa", country);
+          cmd.Parameters.AddWithValue("@Vuosi", year);
+          //suoritetaan kysely kantaan
+          int lkm = cmd.ExecuteNonQuery();
+          conn.Close();
+          return lkm;
+        }
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+    public static int DeleteBook(string connStr, int id)
+    {
+      try
+      {
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+          conn.Open();
+          SqlCommand cmd = new SqlCommand(string.Format("DELETE FROM books WHERE id={0}", id), conn);
+          int lkm = cmd.ExecuteNonQuery();
+          return lkm;
+            
+        }
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+  }
+}
---
+++ IIO11300Harjoitukset/H9BookshopORM/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H9BookshopORM
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void dgBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      //asetetaan stackpanelin datacontekstiksi valittu olio
+      spBook.DataContext = dgBooks.SelectedItem;
+    }
+
+    private void btnHaeTesti_Click(object sender, RoutedEventArgs e)
+    {
+      dgBooks.DataContext = Bookshop.GetTestBooks();
+    }
+
+    private void btnHaeSQLServer_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        //haetaan kirjat BL-kerroksesta
+        dgBooks.DataContext = Bookshop.GetBooks(true);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }    }
+
+    private void btnTallenna_Click(object sender, RoutedEventArgs e)
+    {
+      //mistä tiedetään mitä muokata --> Book-olion ID:stä!
+      try
+      {
+        Book current = (Book)spBook.DataContext;
+        if (Bookshop.UpdateBook(current) > 0)
+        {
+          tbMessage.Text = string.Format("Kirja {0} päivitetty tietokantaan onnistuneesti", current.ToString());
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+
+    }
+
+    private void btnUusi_Click(object sender, RoutedEventArgs e)
+    {
+      if (btnUusi.Content.ToString() == "Uusi")
+      {
+        //luodaan uusi kirja -olio
+        Book newBook = new Book(0);
+        newBook.Name = "Anna kirjan nimi";
+        spBook.DataContext = newBook;
+        btnUusi.Content = "Tallenna uusi kantaan";
+      }
+      else
+      {
+        //tallennetaan
+        Book current = (Book)spBook.DataContext;
+        Bookshop.InsertBook(current);
+        dgBooks.DataContext = Bookshop.GetBooks(true);
+        tbMessage.Text = string.Format("Kirja {0} tallennettu kantaan onnistuneesti", current.ToString());
+        btnUusi.Content = "Uusi";
+      }
+
+    }
+
+    private void btnPoista_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        //poistetaan valittu kirja
+        Book current = (Book)spBook.DataContext;
+        var retval = MessageBox.Show("Haluatko varmasti poistaa kirjan " + current.ToString(), 
+          "Wanhat kirjat kysyy", MessageBoxButton.YesNo);
+        if (retval == MessageBoxResult.Yes)
+        {
+          Bookshop.DeleteBook(current);
+          dgBooks.DataContext = Bookshop.GetBooks(true);
+          tbMessage.Text = string.Format("Kirja {0} poistettu tietokannasta", current.ToString());
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+    }
+  }
+}
