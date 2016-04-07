4c3927900f20bc1c573c46b7c911f5ccd48f023e
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-04-07 08:53:44 +0300

    Test publishing

--- IIO11300Harjoitukset/H10BookShopEF/Properties/AssemblyInfo.cs
+++ IIO11300Harjoitukset/H10BookShopEF/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("H10BookShopEF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BookShop")]
+[assembly: AssemblyDescription("My book shop app")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("H10BookShopEF")]
+[assembly: AssemblyCompany("Mysoft")]
+[assembly: AssemblyProduct("IIO11300 Products")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
